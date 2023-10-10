c83ee275052ee76c8d7aaab2f2c9387e9cdf0324
Phyo Thet Pai <phyo.thet.pai@sji.edu.sg>
2023-10-10 21:13:59 +0800

    Initial Commit

---
+++ Mark Calculator/Mark_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mark_CalculatorApp.swift
+//  Mark Calculator
+//
+//  Created by T Krobot on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Mark_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
